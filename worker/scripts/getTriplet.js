1c9dfb86f1a64030dd0ff597d34da3e811581db1
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-02-01 07:16:13 +0400

    HACK: duplicated `getTriplet()` function

--- worker/scripts/getTriplet.js
+++ worker/scripts/getTriplet.js
@@ -1,3 +1,9 @@
-const { getTriplet } = require('../../node/lib/utils');
+const {arch, platform} = require('os');
+
+// const { getTriplet } = require('../../node/lib/utils');
+function getTriplet()
+{
+	return `${platform()}-${arch()}`;
+}
 
 process.stdout.write(getTriplet());
